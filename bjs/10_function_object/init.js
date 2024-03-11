3b92344745268f3d414254c50df993f2ac79e2c4
Elena-Permyakova <155194052+Elena-Permyakova@users.noreply.github.com>
2024-03-11 21:56:06 +0300

    Update init.js

--- bjs/10_function_object/init.js
+++ bjs/10_function_object/init.js
@@ -1,7 +1,8 @@
-
 window.onload = function()
 {
     const initPerson = personGenerator.getPerson();
+    document.getElementById('surnameOutput').innerText = initPerson.surname;
     document.getElementById('firstNameOutput').innerText = initPerson.firstName;
+    document.getElementById('genderOutput').innerText = initPerson.gender;
+    document.getElementById('birthYearOutput').innerText = initPerson.year;
 };
-
